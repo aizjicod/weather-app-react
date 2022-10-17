591283a80f7b45e8aa979995e0be8606f43c8521
Aiziji <aletv1501@gmail.com>
2022-10-17 01:26:49 -0400

    Change tags order index

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Provider store={store}>
-
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router>
         <App />
-      </Provider>
-    </Router>
-  </React.StrictMode>,
+      </Router>
+    </React.StrictMode>
+    ,
+  </Provider>,
 );
