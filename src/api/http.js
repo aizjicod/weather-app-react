7084285b6905a9335e5e73380d9ed83606a73ea5
Aiziji <aletv1501@gmail.com>
2022-10-17 20:20:43 -0400

    Add 's' at http in order to make secure request

--- src/api/http.js
+++ src/api/http.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const API_KEY = 'M6xaxVTdnOOatMJ6L34QI26W7x1o74Uf';
 
-const getLocationKey = (location) => axios(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${API_KEY}&q=${location}`);
+const getLocationKey = (location) => axios(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${API_KEY}&q=${location}`);
 
-const getForecast = (locationKey) => axios(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${API_KEY}&details=true&metric=true`);
+const getForecast = (locationKey) => axios(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${API_KEY}&details=true&metric=true`);
 
 export default {
   getLocationKey,
